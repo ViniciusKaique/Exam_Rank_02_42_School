95529302996c80c9af65745adff9388f01af5fa5
alexhiguera <alexhigueramartinez@gmail.com>
2024-05-16 16:07:22 +0200

    fix: last_word program ✅

--- Level_2/last_word/last_word.c
+++ Level_2/last_word/last_word.c
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 20:24:22 by columbux          #+#    #+#             */
-/*   Updated: 2024/05/16 14:07:20 by alex             ###   ########.fr       */
+/*   Updated: 2024/05/16 16:06:08 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,12 @@ void	last_word(char *str)
 		while (str[i] == ' ' || str[i] == '\t')
 			--i;
 	}
-	while (str[i] != ' ' && i >= 0)
+	while (str[i] != ' ' && i > 0)
 		i--;
 	while (str[i] != '\0')
 	{
+		if (i == 0)
+			write(1, &str[0], 1);
 		i++;
 		write(1, &str[i], 1);
 	}
